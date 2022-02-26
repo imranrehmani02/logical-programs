128631a533ea05d6c7a1ea482b80da9e6053d079
imranrehmani02 <mo.imranrehmani02@gmail.com>
2022-02-27 00:51:21 +0530

    first-commit

---
+++ armstrong.js
@@ -0,0 +1,39 @@
+
+
+//   Armstrong Number
+
+
+let n=153;
+let t1=n;
+let leng=0;
+
+while(t1!=0)
+{
+	leng = leng+1;
+	t1=parseInt(t1/10);
+}
+
+let t2=n;
+let arm=0;
+let rem;
+
+while(t2!=0)
+{
+	let mul=1;
+	rem=parseInt(t2%10);
+	for(let i=1; i<=leng; i++)
+	{
+		mul = mul * rem;
+	}
+	arm =parseInt(arm+mul);
+	t2=parseInt(t2/10);
+}
+
+if(arm==n)
+{
+	console.log(n+" is armstrong");
+}
+else
+{
+	console.log(n+" is not armstrong");
+}
---
+++ fibonacci.js
@@ -0,0 +1,15 @@
+
+
+//Fibonacci 
+
+
+let a=0,b=1,c;
+console.log(a);
+console.log(b);
+for(let i=1; i<=10;  i++)
+{
+	c=a+b;
+	console.log(c);
+	a=b;
+	b=c;
+}
